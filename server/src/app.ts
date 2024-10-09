382007898ad4dbcbd67042c1580f19254b30bff7
araTabu01 <aratabasum1503@gmail.com>
2024-10-09 14:07:56 +0900

    updated_3

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,32 +13,23 @@ const app = express();
 const ORIGIN1 = process.env.ORIGIN1;
 const ORIGIN2 = process.env.ORIGIN2;
 
-// Filter out undefined values from the origins
-const allowedOrigins = [ORIGIN1, ORIGIN2].filter(
-  (origin): origin is string => origin !== undefined
-);
-
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-// CORS configuration
-const corsOptions = {
-  origin: allowedOrigins, // Use filtered origins
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  credentials: true,
-};
-
-// Enable CORS for all routes
-app.use(cors(corsOptions));
-
-// Respond to OPTIONS requests for preflight checks
-app.options("*", cors(corsOptions));
+/*console.log("First allowed origin is ", ORIGIN1);
+console.log("Second allowed origin is ", ORIGIN2);*/
+app.use(
+  cors({
+    origin: [`${ORIGIN1}`, `${ORIGIN2}`],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "PUT"],
+    credentials: true,
+  })
+);
 
 import stampRoutes from "./routes/stamp.route";
 app.use("/api", stampRoutes);
 
-// Test login route
 app.use("/login", (req, res) => {
   res.send("Hello from the Ara's Backend v2.");
 });
